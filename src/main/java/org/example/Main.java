2941d2dea27d6e8e7cdce0cbffffabbb8bff3a43
Minh Duy Pham <hadtnt71@gmail.com>
2022-12-04 13:07:47 +0700

    find day of week

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,45 @@
 package org.example;
 
+import java.io.*;
+import java.util.Calendar;
 import java.util.Scanner;
 
-public class Main {
-    public static int breadth;
-    public static int height;
-    public static Scanner sc = new Scanner(System.in);
-    static{
-        breadth= sc.nextInt();
-        height = sc.nextInt();
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month-1,day);
+        // In java Docs values of Days of Week
+        // sunday = 1, monday = 2, tuesday = 3, wednesday = 4, thursday = 5,
+        // friday = 6 y saturday = 7
+        String[] daysOfWeek = {"not-zero","SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+
+        int dayExact = cal.get(Calendar.DAY_OF_WEEK);
+        return daysOfWeek[dayExact];
     }
-    public static void main(String[] args) {
-        if(breadth > 0 && height > 0)
-        {
-            System.out.println(breadth*height);
-        }
-        else {
-            System.out.println("java.lang.Exception: Breath and height must be positive");
-        }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+        System.out.println("result "+res);
+//        bufferedWriter.write(res);
+//        bufferedWriter.newLine();
+
+        bufferedReader.close();
+//        bufferedWriter.close();
     }
 }
